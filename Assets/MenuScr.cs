45e593ba3cf984df8830c615988e71a13111d9cc
VadimLNN <svobodolubsev@inbox.ru>
2025-01-26 22:43:05 +0700

    написал скриптик для управления менюшкой

--- Assets/MenuScr.cs
+++ Assets/MenuScr.cs
@@ -1,12 +1,38 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuScr : MonoBehaviour
 {
+    public GameObject menu;
     void Update()
     {
-        if (Input.GetKey(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
-
+            menu.SetActive(true);
+            transform.GetComponent<CursorLock>().unlockCursor();
         }
+    }
+
+    public void CloseMenu()
+    {
+        menu.SetActive(false);
+        transform.GetComponent<CursorLock>().lockCursor();
+    }
+
+    public void LoadScene(string scene)
+    {
+        if (scene != "")
+        {
+            SceneManager.LoadScene(scene);
+        }
+    }
+
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #else
+				        Application.Quit();
+        #endif
     }
 }
